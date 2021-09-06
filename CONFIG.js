10252dadd00490a77a943d63bf82b873af7ad0b1
Sencyan <90180389+Sencyan@users.noreply.github.com>
2021-09-06 16:52:21 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -4,7 +4,7 @@ const CONFIGDATA = {
     "Nếu cậu thoát ra và không trả lời thì có nghĩa là cậu đã ĐỒNG Ý rồi đấy ^^",
   buttonYes: "Đúng rùi, xinh đẹp tuyệt vời nè!!!",
   buttonNo: "Không! Không xinh tí nào:)) .",
-  titleModar: "MÌnh biết mình xinh mà😉🤞",
+  titleModar: "Mình biết mình xinh mà😉🤞",
   descriptionModar:
     'Mình biết các bạn khen thật lòng mà  ^^ </br> Không có chi phải ngại cả! :)) </br> "Đó là sự thật mà, đừng ngại khen mình hihi </br>  Mình thích lắm hihi "',
 };
